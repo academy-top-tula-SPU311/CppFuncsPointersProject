b9be8c54751009cedf1deb1b3322b9696dd49ea7
maxefimov <efimov.top.academy@gmail.com>
2024-03-30 13:10:38 +0300

    Добавьте файлы проекта.

---
+++ CppFuncsPointersProject/CppFuncsPointersProject.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int mult(int a, int b)
+{
+    return a * b;
+}
+
+int calc(int a, int b, int(*operation)(int, int))
+{
+    return operation(a, b);
+}
+
+int(*get_oper(char symbol))(int, int)
+{
+    switch (symbol)
+    {
+    case '+': return sum;
+    case '*': return mult;
+    default:
+        return nullptr;
+    }
+}
+
+int main()
+{
+    auto oper = get_oper('+');
+    std::cout << calc(10, 20, oper) << "\n";
+
+    oper = get_oper('*');
+    std::cout << calc(10, 20, oper) << "\n";
+}
